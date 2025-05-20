2538189dabe7957817817bffba6f43c14f177f67
darshinipriya1 <priyadharshinipriya462@gmail.com>
2025-05-20 13:15:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+st.title("annakodi")   
+st.header("akshu")
+st.subheader("a3")
+st.text("dj")
+st.write("*akshaya*")
+st.write("_sobika_")
+st.title("anu")
+st.write("*annapoorani*")
+st.write("**nagabharathi**")
+st.write("###jeyapriya###")
+st.write("- resma")
+st.write("🧑🏾‍🚀don")
+st.badge("new",color="blue")
+st.image("https://sm.ign.com/t/ign_in/screenshot/default/shin-chan-india-movie_sgq4.2560.jpg")
+with st.sidebar:
+    st.write("id")
+st.code("""
+        a=10
+        print(a)""",language="python")
+
+
